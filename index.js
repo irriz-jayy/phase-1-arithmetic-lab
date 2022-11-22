263c509b1ea6c3cfac87cd34430cba0b21e2a9b1
irriz-jayy <jayfelix789@gmail.com>
2022-11-22 12:22:41 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Multiplication
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+//mod
+let num3 = 19;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+//Max
+const max = (5, 10, 15, 20);
+
+Math.max(max);
+
+//Random
+const random = Math.ceil(Math.random());
+console.log(random);
